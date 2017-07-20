406af941c7a518ff11295ec80e47c4f0694a35db
LiTangqing <tangqinglucy@gmail.com>
2017-07-20 12:41:18 +0800

    deleted env

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/litangqing/Desktop/django-rest-framework-mongoengine-example-master/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
